c67a1421d1626a9dd9ae936cd5a65f2774a900fb
Kaifeng Chen <chenkf@umich.edu>
2017-09-23 22:58:35 -0400

    combine files together

--- Project_1/dropTables.sql
+++ Project_1/dropTables.sql
@@ -1,4 +1,26 @@
+-- first, delete children table
+DROP TABLE FRIENDS;
+
+DROP TABLE USER_CURRENT_CITY;
+
+DROP TABLE USER_HOMETOWN_CITY;
+
+DROP TABLE EDUCATION;
+
 DROP TABLE Messages;
 DROP TABLE Photos;
 DROP TABLE Albums;
 DROP TABLE Tags;
+
+
+-- then, delete parent tables (or we can use CASCADE CONSTRAINTS )
+DROP TABLE USERS;
+
+DROP TABLE CITIES;
+
+DROP TABLE PROGRAMS;
+
+
+
+
+
